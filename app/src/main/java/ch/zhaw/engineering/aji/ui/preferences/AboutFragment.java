c58a9840ce97f97a970837177356d63d138a8bca
Gabriel Nadler <nadlegab@students.zhaw.ch>
2020-05-05 15:29:35 +0200

    Display About page and OSS Licenses

---
+++ app/src/main/java/ch/zhaw/engineering/aji/ui/preferences/AboutFragment.java
@@ -0,0 +1,25 @@
+package ch.zhaw.engineering.aji.ui.preferences;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
+
+import ch.zhaw.engineering.aji.databinding.FragmentAboutBinding;
+
+public class AboutFragment extends Fragment {
+
+    private FragmentAboutBinding mBinding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mBinding = FragmentAboutBinding.inflate(inflater, container, false);
+        return mBinding.getRoot();
+    }
+}
--- app/src/main/java/ch/zhaw/engineering/aji/ui/preferences/PreferenceFragment.java
+++ app/src/main/java/ch/zhaw/engineering/aji/ui/preferences/PreferenceFragment.java
@@ -1,14 +1,51 @@
 package ch.zhaw.engineering.aji.ui.preferences;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.preference.PreferenceFragmentCompat;
 
 import ch.zhaw.engineering.aji.R;
 
 public class PreferenceFragment extends PreferenceFragmentCompat {
+    private PreferenceListener mListener;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.root_preferences);
+
+        findPreference("licenses").setOnPreferenceClickListener(preference -> {
+            mListener.onShowOpenSourceLicenses();
+            return true;
+        });
+        findPreference("about").setOnPreferenceClickListener(preference -> {
+            mListener.onOpenAbout();
+            return true;
+        });
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof PreferenceListener) {
+            mListener = (PreferenceListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement PreferenceListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+
+    public interface PreferenceListener {
+        void onOpenAbout();
+        void onShowOpenSourceLicenses();
     }
 }
