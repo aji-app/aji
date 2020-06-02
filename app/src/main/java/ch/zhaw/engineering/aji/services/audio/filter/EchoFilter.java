f2ed9592446fdd3c4aae2329f30ae4a0ec7a4b10
Gabriel Nadler <gabriel.nadler@gmail.com>
2020-06-02 12:16:29 +0200

    Simple Crackling Fix

--- app/src/main/java/ch/zhaw/engineering/aji/services/audio/filter/EchoFilter.java
+++ app/src/main/java/ch/zhaw/engineering/aji/services/audio/filter/EchoFilter.java
@@ -16,14 +16,15 @@ public class EchoFilter extends AudioBackend.AudioFilter {
     private short[] mEcho;
     private int position = 0;
     private double mEchoStrength;
-    private int mEchoInFrames;
+    private double mEchoInSeconds;
+    private boolean mApplyEcho = false;
 
 
     public EchoFilter(int id, boolean enabled) {
         mId = id;
         setEnabled(enabled);
         setEchoInSeconds(1.0);
-        setEchoStrength(0.3);
+        setEchoStrength(0.4);
     }
 
     public void setEchoStrength(double strength) {
@@ -32,9 +33,9 @@ public class EchoFilter extends AudioBackend.AudioFilter {
 
     public void setEchoInSeconds(double seconds) {
         seconds = Math.max(0.0, Math.min(seconds, 2.0));
-
-        mEchoInFrames = (int) (SAMPLE_RATE * seconds * ASSUMED_CHANNEL_COUNT);
-        mEcho = new short[mEchoInFrames];
+        mEchoInSeconds = seconds;
+        int echoInFrames = (int) (SAMPLE_RATE * seconds * ASSUMED_CHANNEL_COUNT);
+        mEcho = new short[echoInFrames];
     }
 
     @Override
@@ -49,19 +50,30 @@ public class EchoFilter extends AudioBackend.AudioFilter {
 
     @Override
     public byte[] apply(byte[] bytes, AudioBackend.AudioFormat format) {
+        if (format.getBytesPerFrame() / format.getChannelCount() != 2) {
+            Log.w(TAG, "Audio sample has a bigger bit depth than 2 bytes. Filter is now disabled");
+            setEnabled(false);
+            // TODO: Feedback to user
+            return bytes;
+        }
+        Log.i(TAG, String.format("Applying echo filter (%.2fs, %.2f strength) to %d bytes (%d frames) with %d bytes per sample",
+                mEchoInSeconds, mEchoStrength, bytes.length, bytes.length / format.getBytesPerFrame(), format.getBytesPerFrame() / format.getChannelCount()));
         short[] bytesAsShort = bytesToShort(bytes);
         short[] out = bytesAsShort.clone();
 
         for (int i = 0; i < bytesAsShort.length; i++) {
-            out[i] = (short) (bytesAsShort[i] + mEchoStrength * mEcho[position]);
+            if (mApplyEcho) {
+                double value = (bytesAsShort[i] + mEchoStrength * mEcho[position]);
+                out[i] = (short) (value / (1 + mEchoStrength));
+            }
             mEcho[position] = bytesAsShort[i];
             position++;
             if (position == mEcho.length) {
                 position = 0;
+                mApplyEcho = true;
             }
         }
 
-        Log.i(TAG, String.format("Applying noop %d filter to %d bytes (%d frames) with %d bytes per sample", mId, bytes.length, bytes.length / format.getBytesPerFrame(), format.getBytesPerFrame() / format.getChannelCount()));
         return shortsToBytes(out);
     }
 
