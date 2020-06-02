6bc1fd38282cffc2e4fbb36fc2300407406cff39
Gabriel Nadler <gabriel.nadler@gmail.com>
2020-06-02 14:55:34 +0200

    Cleanup and fix tablet handling for filter fragment

--- app/src/main/java/ch/zhaw/engineering/aji/ui/filter/FilterFragment.java
+++ app/src/main/java/ch/zhaw/engineering/aji/ui/filter/FilterFragment.java
@@ -18,55 +18,18 @@ import ch.zhaw.engineering.aji.ui.FabCallbackListener;
 import ch.zhaw.engineering.aji.ui.TabletAwareFragment;
 import ch.zhaw.engineering.aji.util.PreferenceHelper;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FilterFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class FilterFragment extends TabletAwareFragment {
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import static ch.zhaw.engineering.aji.services.audio.AudioService.Filter.EchoFilter;
 
-    private String mParam1;
-    private String mParam2;
+public class FilterFragment extends TabletAwareFragment {
     private FilterFragmentListener mListener;
     private FragmentFilterBinding mBinding;
     private PreferenceHelper mPreferenceHelper;
 
-    public FilterFragment() {
-        // Required empty public constructor
-    }
-
     @Override
     protected void showDetails() {
+
         mAppViewModel.setPlaceholderText(R.string.no_filters);
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FilterFragment.
-     */
-    public static FilterFragment newInstance(String param1, String param2) {
-        FilterFragment fragment = new FilterFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        mListener.onFilterSelected(EchoFilter);
     }
 
     @Override
@@ -74,7 +37,7 @@ public class FilterFragment extends TabletAwareFragment {
         mBinding = FragmentFilterBinding.inflate(inflater, container, false);
 
         mBinding.echoFilter.setOnClickListener(v -> {
-            mListener.onFilterSelected(AudioService.Filter.EchoFilter);
+            mListener.onFilterSelected(EchoFilter);
         });
         // Inflate the layout for this fragment
         return mBinding.getRoot();
@@ -86,7 +49,6 @@ public class FilterFragment extends TabletAwareFragment {
         if (context instanceof FilterFragmentListener) {
             mListener = (FilterFragmentListener) context;
             mListener.disableFab();
-            mListener.showEmptyDetails();
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement FilterFragmentListener");
